8feac6457d2a2c9b06469df1dcf81f32ca2882ca
junggunlee <leejunggun7@gmail.com>
2017-10-17 16:09:09 +0900

    make search function with dates

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,8 @@
 class PetsController < ApplicationController
 
   def index
-    @pets = Pet.species("dog")
+    @start_date = DateTime.new(2017, 10, 5)
+    @end_date = DateTime.new(2017, 10, 7)
+    @pets = Pet.species("pokemon").start_date(@start_date).end_date(@end_date)
   end
 end
