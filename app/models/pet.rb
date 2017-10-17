f3f577803b6c83da559b5331f22174c52e79c6f3
junggunlee <leejunggun7@gmail.com>
2017-10-17 20:04:34 +0900

    make booking page

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,6 +1,8 @@
 class Pet < ApplicationRecord
   # belongs_to :user
   belongs_to :owner, class_name: "User", foreign_key: "owner_id"
+  has_many :bookings, dependent: :destroy
+
   validates :name, :species, :description, presence: true
   validates :name, uniqueness: true
 
