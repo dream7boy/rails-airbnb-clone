ad053591bb65fd6b9b5f190fb22b744d63991d23
Debbie Jong <surrealstardust@gmail.com>
2017-10-19 18:54:48 +0900

    total cost shows error message now instead of negative numbers when user puts end date before start date

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,12 +20,15 @@ $('#rental-start-date, #rental-end-date').change(function(event) {
 
 
   const days = (end - start) / (1000*60*60*24) + 1;
-
+  const daily_price = $('#total-cost').data('price');
   if (days < 1) {
     event.preventDefault();
     alert('Error! Please make sure your end date is after your start date.');
+    $('#total-cost').text(`ERROR: Please make sure your end date is after your start date.`);
+  } else {
+    $('#total-cost').text(`¥ ${daily_price} * ${days} day${days > 1 ? 's' : ''} = ¥ ${daily_price * days}`);
   }
-  const daily_price = $('#total-cost').data('price');
+
   // console.log(daily_price * days);
 
   // ¥ 16000 * 2 days = ¥ 32000
@@ -35,5 +38,4 @@ $('#rental-start-date, #rental-end-date').change(function(event) {
   // } else {
   //   days_plurality = "day";
   // }
-  $('#total-cost').text(`¥ ${daily_price} * ${days} day${days > 1 ? 's' : ''} = ¥ ${daily_price * days}`);
 });
