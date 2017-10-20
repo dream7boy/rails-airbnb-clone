dd78f7c9f967ebc73dbca421f6639142b070b31a
junggunlee <leejunggun7@gmail.com>
2017-10-20 11:22:09 +0900

    edit js file again

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,20 +8,19 @@
 
 
 $('#rental-start-date, #rental-end-date').change(function(event) {
-  alert("Hi there!")
-  var end_date = $('#rental-end-date').val();
-  var start_date = $('#rental-start-date').val();
-  var start_date_arr = start_date.split('-');
-  var start = new Date(start_date_arr[0], parseInt(start_date_arr[1]) - 1, start_date_arr[2]);
-  var end_date_arr = end_date.split('-');
-  var end = new Date(end_date_arr[0], parseInt(end_date_arr[1]) - 1, end_date_arr[2]);
-  var days = (end - start) / (1000*60*60*24) + 1;
-  var daily_price = $('#total-cost').data('price');
+  const end_date = $('#rental-end-date').val();
+  const start_date = $('#rental-start-date').val();
+  const start_date_arr = start_date.split('-');
+  const start = new Date(start_date_arr[0], parseInt(start_date_arr[1]) - 1, start_date_arr[2]);
+  const end_date_arr = end_date.split('-');
+  const end = new Date(end_date_arr[0], parseInt(end_date_arr[1]) - 1, end_date_arr[2]);
+  const days = (end - start) / (1000*60*60*24) + 1;
+  const daily_price = $('#total-cost').data('price');
   if (days < 1) {
     event.preventDefault();
     alert('Error! Please make sure your end date is after your start date.');
     $('#total-cost').text('ERROR: Please make sure your end date is after your start date.');
   } else {
-    // $('#total-cost').text(`¥ ${daily_price} * ${days} day${days > 1 ? 's' : ''} = ¥ ${daily_price * days}`);
+    $('#total-cost').text(`¥ ${daily_price} * ${days} day${days > 1 ? 's' : ''} = ¥ ${daily_price * days}`);
   }
 });
