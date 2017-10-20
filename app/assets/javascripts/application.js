7da64da55ab5842aa9b29aa07cc78939098be008
Debbie Jong <surrealstardust@gmail.com>
2017-10-20 10:53:26 +0900

    fix javascript files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,37 +5,3 @@
 //= require underscore
 //= require gmaps/google
 //= require_tree .
-
-
-$('#rental-start-date, #rental-end-date').change(function(event) {
-  const end_date = $('#rental-end-date').val();
-  const start_date = $('#rental-start-date').val();
-  // console.log(end_date, start_date);
-  // 2017-10-27
-  const [year_start, month_start, day_start] = start_date.split('-');
-  const start = new Date(year_start, parseInt(month_start) - 1, day_start);
-
-  const [year_end, month_end, day_end] = end_date.split('-');
-  const end = new Date(year_end, parseInt(month_end) - 1, day_end);
-
-
-  const days = (end - start) / (1000*60*60*24) + 1;
-  const daily_price = $('#total-cost').data('price');
-  if (days < 1) {
-    event.preventDefault();
-    alert('Error! Please make sure your end date is after your start date.');
-    $('#total-cost').text(`ERROR: Please make sure your end date is after your start date.`);
-  } else {
-    $('#total-cost').text(`¥ ${daily_price} * ${days} day${days > 1 ? 's' : ''} = ¥ ${daily_price * days}`);
-  }
-
-  // console.log(daily_price * days);
-
-  // ¥ 16000 * 2 days = ¥ 32000
-  // var days_plurality;
-  // if (days > 1) {
-  //   days_plurality = "days";
-  // } else {
-  //   days_plurality = "day";
-  // }
-});
